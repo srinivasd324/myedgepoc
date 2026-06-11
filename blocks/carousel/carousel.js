a93e7b25a64dd704245b4db7ba14c589fefc8e68
Daram <srinivas.rao.daram@accenture.com>
2026-06-11 18:05:29 +0530

    Coarousel code added

---
+++ blocks/carousel/carousel.js
@@ -0,0 +1,9 @@
+export default function decorate(block) {
+    [...block.children].forEach((row,rindex) => {
+        if(index==0){
+            const button = document.createElement("button");
+
+        }
+    });
+    
+}
