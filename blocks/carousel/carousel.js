011d9ee60ae63d7a68c13aaad0573834b0902367
srinivasd324 <srinivasd324@gmail.com>
2026-06-11 18:19:53 +0530

    Update carousel.js

--- blocks/carousel/carousel.js
+++ blocks/carousel/carousel.js
@@ -11,14 +11,14 @@ export default function decorate(block) {
           const nextbtn = document.createElement('button');
           nextbtn.classList.add('btn');
           nextbtn.classList.add('btn-next');
-         const node = document.createTextNode(btnNxt);
+         const node = document.createTextNode(row.textContent);
          nextbtn.append(node);
          row.replaceWith(nextbtn);
       }else if(r==rows.length-1){
           const prebtn = document.createElement('button');
           prebtn.classList.add('btn');
           prebtn.classList.add('btn-prev');
-          const node = document.createTextNode(btnPre);
+          const node = document.createTextNode(row.textContent);
           prebtn.append(node);
           row.replaceWith(prebtn);
       }else{
